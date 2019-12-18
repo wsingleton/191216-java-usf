354df44f219d62126ab16b107206b1a2a91eab43
unknown <jaredallencarterjac@gmail.com>
2019-12-18 16:17:09 -0500

    StringDriver

---
+++ jared_carter_Code/Week_A/day3-demos/src/com/revature/StringDriver.java
@@ -0,0 +1,17 @@
+package com.revature;
+import static java.lang.System.out;
+public class StringDriver {
+
+    public static void main(String[] args){
+        String s1 = "North";
+        String s2 = "North";
+        String s3 = new String("North");
+        String s4 = null;
+
+
+
+
+
+
+    }
+}
