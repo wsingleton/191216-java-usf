016a8d199d39bef08c4452412172cd65eb596812
Anthony Smith <amsmithjr1989@gmail.com>
2019-12-18 14:57:58 -0500

    pushing demos

--- anthony_smith_Code/week_A/Day3-demos/src/com/revature/ArrayDriver.java
+++ anthony_smith_Code/week_A/Day3-demos/src/com/revature/ArrayDriver.java
@@ -1,6 +1,50 @@
 package com.revature;
 
+import java.util.Arrays;
+
+import static java.util.Arrays.*;
+
 public class ArrayDriver {
 
-    
+    public static void main(String[] args){
+
+        int[] intArray =  new int[10];
+        System.out.println("String representation of intArray: " + intArray);
+        System.out.println("length of array " + intArray.length);
+
+        printValues(intArray);
+
+        intArray[0] = 99;
+        intArray[intArray.length-1] = 42;
+
+        printValues(intArray);
+
+        // System.out.println(intArray[123]);
+
+        int[] otherInts = copyOf(intArray,  intArray.length + 1);
+
+        System.out.println("Length of intArray: " + intArray.length);
+        System.out.println("Length of otherInts: " + otherInts.length);
+
+        int[] anthoerArray = {new Integer(44)}; // unboxing
+        Integer[] integerArray = {123}; // autoboxing
+
+
+
+    }
+
+    private static void printValues(int[] intArray) {
+    for(int i = 0; i < intArray.length; i++){
+        System.out.println("value at index position " + i + ":" + intArray[i]);
+         }
+    }
+
+    private static void printValues(String[] stringArray) {
+        //for-each loop (aka enhanced for loop)
+        for (String s :stringArray){
+            System.out.println(s);
+        }
+    }
+
+
 }
---
+++ anthony_smith_Code/week_A/Day3-demos/src/com/revature/VarArgsDriver.java
@@ -0,0 +1,41 @@
+package com.revature;
+
+public class VarArgsDriver {
+
+
+    //The main method can use variable arguments and still be considered valid syntactically
+    public static void main(String[] args) {
+
+        varArgs("test");
+        varArgs("test2", 1);
+        varArgs("test3", 1,2,3);
+    }
+
+    /*
+    Variable arguments must be the last parameter in the parameters list!
+     */
+
+
+    public static void varArgs(String myString, int... values){
+        System.out.println(myString);
+
+        for (int i : values) {
+            System.out.println(i);
+        }
+
+    }
+
+    /*
+     we can make our variable arguments as many dimensions as desired,
+     just add a set of square of a square brackets and the ellipses to pass
+     in a multi-dimensional
+     */
+    public static void show(int[]... my2dArray){
+
+        for(int arr : my2dArray){
+            for (int: arr){
+                System.out.println((i));
+            }
+        }
+    }
+}
