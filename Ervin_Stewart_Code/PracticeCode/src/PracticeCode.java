7cef703323dfcc3fa00ed1b0e53ee50ebaa3c963
ervin stewart <epluribusfungo@gmail.com>
2019-12-18 11:39:50 -0500

    git ignore update

---
+++ Ervin_Stewart_Code/PracticeCode/src/PracticeCode.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class PracticeCode {
+
+
+
+
+
+public class UserID{
+    private int year;
+    private boolean isLeapYear;
+    private boolean isfemale;
+    private boolean ismale;
+    private int age;
+    private int day;
+    private int month;
+    private String gender;
+    private String Birthday;
+    private String firstname;
+    private String Password;
+    private String UserName;
+
+   public void UserID(String , String UserName, int age) {
+       this.Birthday = Birthday;
+       this.age = age;
+   }
+   public String generateUsername(String firstname, String lastname){
+
+   }
+}
+}
