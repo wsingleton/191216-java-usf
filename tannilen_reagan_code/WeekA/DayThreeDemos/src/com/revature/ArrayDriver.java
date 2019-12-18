43a551de21be139964f8b5ca0d22a38ef404931a
Tannilen Reagan <tannilen.reagan@gmail.com>
2019-12-18 14:56:52 -0500

    Pushing Array and VarArgs demos

--- tannilen_reagan_code/WeekA/DayThreeDemos/src/com/revature/ArrayDriver.java
+++ tannilen_reagan_code/WeekA/DayThreeDemos/src/com/revature/ArrayDriver.java
@@ -1,5 +1,52 @@
 package com.revature;
 
-public class ArrayDriver {
-    
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayDriver<T> {
+    public static void main(String[] args) {
+        //create Array
+        int[] intArray = new int[10];
+        //output Array detail
+        System.out.println("String representation of intArray: " + intArray);
+        System.out.println("Length of intArray: " + intArray.length);
+        //output values of intArray
+        printValues(intArray);
+        //set first and last values of intArray
+        intArray[0]=93;
+        intArray[intArray.length-1]=42;
+        //output values of intArray
+        printValues(intArray);
+        //alternative array instantiation syntax
+        double doubleVals[] = new double[5];
+        //array literal sample
+        String[] myStrings={"this", "is", "an", "array", new String("literal")};
+        //output values of myStrings
+        printValues(myStrings);
+        //Array duplication
+        int[] otherInts = Arrays.copyOf(intArray, intArray.length+1);
+        printValues(otherInts);
+        //length comparison confirmation
+        System.out.println("Length of intArray: " + intArray.length);
+        System.out.println("Length of otherInts: " + otherInts.length);
+        //Object Array example - can include a variety of different types, will automatically "box" them
+        Object[] randomStuff={12, "test", new ArrayList<Boolean>(), true, 4.3};
+        //Java will also "unbox"
+        int[] anotherArray={new Integer(44)};
+        //intentionally throw an ArrayIndexOutOfBoundsException, line converted to comment to eliminate exception without removal
+        //System.out.println(intArray[123]);
+    }
+
+    private static void printValues(int[] intArray) {
+        //Traditional for loop
+        for (int i=0; i<intArray.length; i++) {
+            System.out.println("Value at index position " + i + ": " + intArray[i]);
+        }
+    }
+    private static void printValues(String[] stringArray) {
+        //For-each or Enhanced for loop
+        for (String s : stringArray) {
+            System.out.println(s);
+        }
+    }
 }
---
+++ tannilen_reagan_code/WeekA/DayThreeDemos/src/com/revature/VarArgsDriver.java
@@ -0,0 +1,55 @@
+package com.revature;
+
+public class VarArgsDriver {
+    //The main method may use variable arguments and still be considered valid syntactically.
+    public static void main(String... args) {
+        //sample functionality of variable arguments in action
+        varArgs("test");
+        varArgs("test2", 1);
+        varArgs("test3",1,2,3);
+        //instantiating a 2-d array using array literals
+        int[][] twoDimArr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        //calling the showVals method
+        showVals(twoDimArr);
+        String[][] associates191216 = {
+                {"Wezley","Singleton"},
+                {"Tannilen","Reagan"},
+                {"Alexis","Lindsay"},
+                {"Andrew","Spiteri"},
+                {"Anitra","McCain"},
+                {"Anthony","Smith"},
+                {"Benjamin","Burke"},
+                {"Blake","Dunn"},
+                {"Dayvon","Allen"},
+                {"Ervin","Stewart"},
+                {"Evan","Hsi"}
+        };
+    }
+    /*  Variable arguments must be the last parameter in the parameters list.
+        There can only be one variable argument parameter in a method.*/
+    public static void varArgs(String myString, int... values) {
+        System.out.println(myString);
+        for (int i : values) {
+            System.out.println(i);
+        }
+    }
+    /* Functionality of multidimensional arrays in for-loops */
+    public static void showVals(int[]... my2dArray){
+        for(int[] arr: my2dArray) {
+            for (int i : arr) {
+                System.out.println(i);
+            }
+        }
+    }
+    public static void showVals(String[]... my2dArray){
+        for(String[] arr: my2dArray) {
+            for (String i : arr) {
+                System.out.println(i);
+            }
+        }
+    }
+}
