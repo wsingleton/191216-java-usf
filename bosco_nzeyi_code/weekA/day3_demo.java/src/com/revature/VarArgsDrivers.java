c67eada48d7eeaac2a4a20062b00695a142b31d4
Bosco <nzibosco@gmail.com>
2019-12-18 14:20:53 -0500

    another advanced array

--- bosco_nzeyi_code/weekA/day3_demo.java/src/com/revature/VarArgsDrivers.java
+++ bosco_nzeyi_code/weekA/day3_demo.java/src/com/revature/VarArgsDrivers.java
@@ -1,4 +1,21 @@
 package com.revature;
 
-public class VarArgsDriversls {
+public class VarArgsDrivers {
+
+    public static void main(String[] args){
+
+        varArgs("test");
+        varArgs("test2", 1);
+        varArgs("test3", 1,3,5);
+    }
+
+// we can ... in place of [] to declare an array. They have the same behaviors.
+
+    public static void varArgs(String myString, int... values){
+
+        System.out.println(myString);
+        for (int i: values){
+            System.out.println(i);
+        }
+    }
 }
