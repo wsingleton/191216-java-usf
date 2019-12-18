39a94f4e57f9a8d9378c56ca2c9e733f7e6c80b7
Ben <benjaminstevenburke@gmail.com>
2019-12-18 14:57:54 -0500

    pushing arrays

---
+++ benjamin_burke_code/week_A/day 3/src/VarArgsDriver/VarArgsDriver.java
@@ -0,0 +1,60 @@
+package VarArgsDriver;
+
+public class VarArgsDriver {
+
+    /*
+    the main method can use variable arguments and still be considered valid syntactically
+    form of polymorphism-method overriding
+    logic the same, there's a better way of doing it,
+    overload if the logic is different!
+
+     */
+
+    public static void main(String... args){
+//    public static void main(String[] args){
+
+        varArgs("test");
+        varArgs("test2", 1);
+        varArgs( "test3", 1, 2, 3);
+
+
+    }
+
+    /*
+        Variables arguments must be the last parameter in the parameters list!
+
+    */
+
+    public static void varArgs(String myString, int... values){
+        System.out.println(myString);
+
+        for (int i : values){
+            System.out.println(i);
+        }
+    }
+
+    /*
+    We can make our variable arguments as many dimensions as desired as desired, just add a set of square brackets
+    and the ellipses to pass in a multi-d array.
+
+
+
+     */
+    public static void show(int[]... my2dArray){
+
+        for (int i=0; i< my2dArray.length; i++){
+            for (int j=0; j < my2dArray[i].length; j++){
+                System.out.println(my2dArray[i][j]);
+            }
+        }
+
+
+        System.out.println("-------------------------");
+
+        for (int[] arr : my2dArray){
+            for(int i: arr ) {
+                System.out.println(i);
+            }
+        }
+    }
+}
--- benjamin_burke_code/week_A/day 3/src/com/revature/ArrayDriver.java
+++ benjamin_burke_code/week_A/day 3/src/com/revature/ArrayDriver.java
@@ -1,6 +1,9 @@
 package com.revature;
 
-public class ArrayDriver {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayDriver<T> {
 
     public static void main(String[] args) {
         int[] intArray = new int[10];
@@ -15,9 +18,30 @@ public class ArrayDriver {
 
         printvalues(intArray);
 
+        // an instantiation using an array literal
+        double doublevals[] = new double[5]; //legal way to do it
+
+        String[] myStrings = {"this", "is ", "an", "array", "literal", new String("literal")};
+        String s="test";
+
+        printvalues(myStrings);
+
+
+        int[] otherInts = Arrays.copyOf(intArray, intArray.length+1);
+        printvalues(otherInts);
+
+        System.out.println("length of intArray: " + intArray.length);
+        System.out.println("lenght of otherInt: " + otherInts.length);
+
+        int[] anotherArray = { new Integer(44)};
+        Integer[] integerArray = { 123 };
+
+        //java can know what you mean
+        System.out.println(new Integer(4) + new Integer(4));
+
+        Object[] randomStuff = { 12, "test", new ArrayList<Boolean>(), true, 4.3};
+
         System.out.println(intArray[123]);//throws an exception
-
-
     }
 
     private static void printvalues(int[] intArray) {
@@ -25,4 +49,11 @@ public class ArrayDriver {
             System.out.println("value at index position: " + i + ": " + intArray[i]);
         }
     }
+    //for-each loop (aka enhanced for loop)
+    private static void printvalues(String[] stringArray) {
+        for (String s : stringArray){
+            System.out.println(s);
+        }
+    }
+
 }
