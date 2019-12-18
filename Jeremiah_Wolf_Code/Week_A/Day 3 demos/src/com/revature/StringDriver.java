2f8b7fd5c64b3ef86a72b1b105401fbbfcb9863f
Jeremiah Wolf <jeremiahthomaswolf@gmail.com>
2019-12-18 16:14:22 -0500

    adding string driver

---
+++ Jeremiah_Wolf_Code/Week_A/Day 3 demos/src/com/revature/StringDriver.java
@@ -0,0 +1,15 @@
+package com.revature;
+
+public class StringDriver {
+
+    public static void main(String[] args) {
+        String s1 = "North";
+        String s2 = "North";
+        String s3 = new String ("String");
+        String s4 = null;
+;
+        System.out.println(s1 == s2);
+        System.out.println(s1 ==s3);
+
+    }
+}
