b76c912f12bb02d454f8c3a2af366ecc872509e5
Ben <benjaminstevenburke@gmail.com>
2019-12-18 13:11:19 -0500

    updating the lecture

--- benjamin_burke_code/week_A/day 3/src/com/revature/ArrayDriver.java
+++ benjamin_burke_code/week_A/day 3/src/com/revature/ArrayDriver.java
@@ -1,4 +1,28 @@
 package com.revature;
 
 public class ArrayDriver {
+
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        System.out.println("Here is the array: " + intArray);
+        System.out.println("Length of intArray: " + intArray.length);
+
+        printvalues(intArray);
+
+
+        intArray[0] = 93;
+        intArray[intArray.length-1] = 42;
+
+        printvalues(intArray);
+
+        System.out.println(intArray[123]);//throws an exception
+
+
+    }
+
+    private static void printvalues(int[] intArray) {
+        for (int i =0; i<intArray.length; i++){
+            System.out.println("value at index position: " + i + ": " + intArray[i]);
+        }
+    }
 }
