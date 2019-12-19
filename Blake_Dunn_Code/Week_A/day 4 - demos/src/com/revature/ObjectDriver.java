b5ee399b7c1744313f3b767dce13569b0d963c62
Blake Dunn <bdunn@DESKTOP-9VJVNCN>
2019-12-19 15:33:41 -0500

    push day4 - demos changes

--- Blake_Dunn_Code/Week_A/day 4 - demos/src/com/revature/ObjectDriver.java
+++ Blake_Dunn_Code/Week_A/day 4 - demos/src/com/revature/ObjectDriver.java
@@ -1,8 +1,33 @@
 package com.revature;
 
+import com.revature.models.User;
+import com.revature.models.Role;
+
 public class ObjectDriver {
 
     public static void main(String[] args) {
+
+        // Fully parameterized constructor call
+        User myUser = new User(1, "Alice", "Anderson", "a.anderson", "p4sswo0rd", Role.ADMIN);
+
+        // Partially parameterized constructor call
+        User otherUser = new User("Bob", "Bailey", "b.bailey", "password", Role.MEMBER);
+
+        // No args constructor call
+        User defaultUser = new User();
+
+        System.out.println(myUser);
+        System.out.println(myUser.hashCode());
+
+        System.out.println(myUser.getFirstName());
+
+        String userInput = "alice.anderson";
+        myUser.setUsername(userInput);
+        System.out.println(myUser);
+
+        User aliceClone = new User(1, "Alice", "Anderson", "alice.anderson", "p4ssw0rd", Role.ADMIN);
+        System.out.println(myUser == aliceClone); // checking for memory equivalence (is same object)
+        System.out.println(myUser.equals(aliceClone)); //checking for value equivalence
 
 
     }
---
+++ Blake_Dunn_Code/Week_A/day 4 - demos/src/com/revature/OverloadDriver.java
@@ -0,0 +1,28 @@
+package com.revature;
+
+import com.revature.util.Overloader;
+
+public class OverloadDriver {
+
+    public static void main(String[] args) {
+
+        Overloader overloader = new Overloader();
+        overloader.method(1);                   // int
+        overloader.method(1L);                  // long
+        overloader.method();                       // variable arguments
+        overloader.method(1, 2, 3);            // variable arguments
+//        overloader.method(null);                 // compilation error (ambiguous type)
+        overloader.method(new Integer(65));  // Integer
+        overloader.method(3.14);                // double
+        overloader.method(3.14f);               // float
+        overloader.method(new Double(2.9));  // Double
+        overloader.method(new Float(2.9));   // Float
+
+        // variable arguments (with unboxing!)
+        overloader.method(new Integer(4), new Integer(5));
+
+
+
+    }
+
+}
