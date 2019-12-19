fa47c439c64313adbfd627de003c89c4e6cc9ffb
Ben <benjaminstevenburke@gmail.com>
2019-12-19 11:21:59 -0500

    adding day 4

---
+++ benjamin_burke_code/week_A/day4_demos/src/com/revature/ObjectDriver.java
@@ -0,0 +1,10 @@
+package com.revature;
+import com.revature.models.User;
+
+public class ObjectDriver {
+
+    public static void main(String[] args){
+
+
+    }
+}
---
+++ benjamin_burke_code/week_A/day4_demos/src/com/revature/models/Role.java
@@ -0,0 +1,10 @@
+package com.revature.models;
+
+public enum Role {
+
+    ADMIN, DEV, TESTER, MEMBER;
+
+
+
+
+}
