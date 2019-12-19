4e487323cfd324aa30bebbe28ce6503ef9098df9
unknown <jaredallencarterjac@gmail.com>
2019-12-19 15:45:38 -0500

    day4.2

--- jared_carter_Code/Week_A/day4-demos/src/com/revature/ObjectDriver.java
+++ jared_carter_Code/Week_A/day4-demos/src/com/revature/ObjectDriver.java
@@ -1,7 +1,11 @@
 package com.revature;
 
+import com.revature.models.Role;
+import com.revature.models.User;
+
 public class ObjectDriver {
     public static void main(String[] args){
 
+        User myUser = new User(1,"Alice", "Anderson","a.anderson", Role.MEMBER,"bang");
     }
 }
