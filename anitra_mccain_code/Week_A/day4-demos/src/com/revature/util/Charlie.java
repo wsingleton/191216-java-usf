3a596de7994c03053515f9c3012eadeea03e5f18
Anitra <anitra.oursky@gmail.com>
2019-12-19 16:43:06 -0500

    Add Overload and Override Driver

---
+++ anitra_mccain_code/Week_A/day4-demos/src/com/revature/util/Charlie.java
@@ -0,0 +1,54 @@
+package com.revature.util;
+
+import com.revature.models.Alpha; // need to import since it is in another package
+import com.revature.models.Circle;
+import com.revature.models.Shape;
+
+public class Charlie extends Alpha {
+
+    //Package-private members are not visible beyond the package
+    //in which they are declared - even for subclasses
+//    @Override
+//    String packageStringGetter() {
+//
+//    }
+
+    //protected members are visible to all subclasses, even if they
+    //are located in a different package
+    @Override
+    protected double protectedDoubleGetter() {
+        return 3.14;
+    }
+
+    //You cannot override a method by replacing the return type with
+    //a completely different type
+//    @Override
+//    public String getCircle() {
+//        return "";
+//    }
+
+    // We cannot make the return type of the method a parent class of the
+    //inherited method
+//    @Override
+//    public Shape getCircle() {
+//        return new Shape();
+//    }
+
+    //Valid, return types can be changed to a subclass of the original
+    //return type of the inherited method.
+    @Override
+    public Circle getShape() {
+        return new Circle();
+    }
+
+    //You cannot modify the parameter types of inherited methods
+//    @Override
+//    public void setShape(Circle circle) {
+//
+//    }
+//
+//    @Override
+//    public void setCircle(Shape shape) {
+//
+//    }
+}
