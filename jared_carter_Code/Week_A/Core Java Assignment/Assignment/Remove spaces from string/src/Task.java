35c5347c3fdc3658991f4ea43262dba07c120a2f
unknown <jaredallencarterjac@gmail.com>
2019-12-19 17:09:33 -0500

    done

---
+++ jared_carter_Code/Week_A/Core Java Assignment/Assignment/Remove spaces from string/src/Task.java
@@ -0,0 +1,10 @@
+public class Task {
+
+    public String collapseWhiteSpace(String str) {
+
+        if (str == null || str.trim().equals("")) return "";
+        return str.replaceAll(" ", "");
+
+    }
+
+}
