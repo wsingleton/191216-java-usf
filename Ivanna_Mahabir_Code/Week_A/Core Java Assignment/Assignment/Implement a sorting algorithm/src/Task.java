9d682ca2d63abb192d0eb0d230b50bda9566616b
Ivanna Mahabir <iv.mahabir@gmail.com>
2019-12-19 19:01:31 -0500

    read consol driver

---
+++ Ivanna_Mahabir_Code/Week_A/Core Java Assignment/Assignment/Implement a sorting algorithm/src/Task.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Task {
+
+    public int[] sortNumbers(int[] numbersForSorting) {
+
+        // Check the base cases
+        if(numbersForSorting == null || numbersForSorting.length == 0) {
+            return new int[0];
+        }
+
+        // Make a copy of the original array, this copy will be sorted instead (functional)
+        int[] arr = Arrays.copyOf(numbersForSorting, numbersForSorting.length);
+
+        // Sort the array
+        int n = arr.length;
+        IntStream.range(0, n - 1)
+                 .flatMap(i -> IntStream.range(1, n - i))
+                 .forEach(j -> {
+                    if(arr[j -1] > arr[j]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = temp;
+                    }
+                 });
+
+        return arr;
+
+    }
+}
---
+++ Ivanna_Mahabir_Code/Week_A/day4-demos/src/com/revature/ReadConsoleDriver.java
@@ -0,0 +1,14 @@
+package com.revature;
+
+public class ReadConsoleDriver {
+
+    public static void main(String[] args){
+
+        //scanner or buffer reader class to read from the console
+        //scanner is good for parse data
+        //buffer reader is just for reading strings
+
+        
+    }
+
+}
