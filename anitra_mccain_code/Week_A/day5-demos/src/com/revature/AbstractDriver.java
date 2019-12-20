5d16f70b6e92f427a22aa735fdef4910468e42bd
Anitra <anitra.oursky@gmail.com>
2019-12-20 14:19:32 -0500

    Add Abstract driver

---
+++ anitra_mccain_code/Week_A/day5-demos/src/com/revature/AbstractDriver.java
@@ -0,0 +1,35 @@
+package com.revature;
+
+import com.revature.models.Animal;
+import com.revature.models.Cat;
+
+public class AbstractDriver {
+
+    public static void main(String[] args){
+
+        Cat myCat = new Cat();
+        Cat yourCat = new Cat(8, "Sphinx", false);
+        Cat thisCat = new Cat(5, "Persian", true);
+        Cat herCat = new Cat(7, "Serval", true);
+
+        Animal myAnimal = new Cat(9, "Himalayan", true);
+//        Animal noAnimal = new Animal(); //abstract classes cannot be directly instantiated
+
+        System.out.println(myCat.getBreed());
+        System.out.print("-----------------------------------------------------");
+        System.out.println(yourCat.isHasFur());
+        System.out.print("*****************************************************");
+        System.out.println(thisCat.isHasFur());
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        herCat.makeSound();
+
+        /*
+        * Covariance is when you have a reference of a super type that points to an object of a subtype.
+        * This reference will have access to only the states and behaviors of the super type. Although,
+        * if any methods of the parent are overridden by the subtype, those methods will behave differently.
+        * */
+
+        System.out.println(myAnimal.numberOfLives); //covariance
+
+    }
+}
