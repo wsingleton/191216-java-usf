55e1cc6b7346099719fe8a1339f97f7de76bdb9e
Bosco <nzibosco@gmail.com>
2019-12-20 16:41:39 -0500

    collections

---
+++ bosco_nzeyi_code/weekA/day5_demo/src/com/revature/CollectionDriver.java
@@ -0,0 +1,28 @@
+package com.revature;
+
+/*
+Collection API follow the following diagram:
+
+                                            Iterable
+                                                |
+                                            Collection
+
+                                            /    |    \
+                                       List    Set   Queue
+                                                       ^
+                                                       |
+                                                     Deque
+In List: We have ArrayList, LinkedList, Vectors, and Stacks.
+in sets we have: hashSet, TreeSet.
+In Queue we have: Priority Queue,
+
+In Deck: ArrayDeque.
+
+ */
+
+public class CollectionDriver {
+
+    public static void main (String [] args){
+
+    }
+}
