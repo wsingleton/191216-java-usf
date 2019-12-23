b1c64eca82a9428e49ab8ee17bf18c1003faf4c1
Niles Diggs <ndiggs@wesleyan.edu>
2019-12-23 12:18:46 -0500

    core java assignments

--- niles_diggs_code/Week_B/Core Java/Basic IO/Writing to a file/src/Task.java
+++ niles_diggs_code/Week_B/Core Java/Basic IO/Writing to a file/src/Task.java
@@ -5,12 +5,38 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Task {
 
     public void writeToFile(String[] linesToWrite) {
 
-        // Provide your implementation here
+        // first check to see if this is valid for use
+        if (linesToWrite == null || linesToWrite.length == 0) {
+            return;
+        }
+
+        //Wrap risky logic in try catch and write lines to a file
+        try {
+            // Create the file which will be written to
+            File writeFile = new File("resources/write.txt");
+            if(!writeFile.exists()) writeFile.createNewFile();
+
+            //Define a filepath for the output
+            Path outPath = Paths.get("resources/write.txt");
+
+            // Convert the String array into a List for use with file methods
+            List<String> linesArray = Arrays.asList(linesToWrite);
+
+            // Write the lines to the file
+            Files.write(outPath, linesArray, Charset.defaultCharset());
+)
+        }
+        // handle exceptions
+        catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("An unexpected error occurred.");
+        }
 
     }
 
