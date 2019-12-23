9c08b53a74390b3a877bb913cf85dc57e2749b0d
Jeremiah Wolf <jeremiahthomaswolf@gmail.com>
2019-12-23 14:20:04 -0500

    write file example not completed

--- Jeremiah_Wolf_Code/Week B/src/com/revature/WriteFileDriver.java
+++ Jeremiah_Wolf_Code/Week B/src/com/revature/WriteFileDriver.java
@@ -1,7 +1,30 @@
 package com.revature;
 
+import com.revature.models.User;
+
+import java.io.*;
+
 public class WriteFileDriver {
 
-    
+    public static void main(String[] args) {
+
+        File writeFile = new File("src/resources/users.txt");
+
+        //try-with-resources (intoduced in Java 7) :: auto closes objects declared as resources
+        // only allows for the instantiation of objects that implemnt AutoCloseable interface
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(writeFile)));
+        {
+
+            User u = new User(4, "rconnell", "java");
+            writer.write("\n" + u.toFileString());
+
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
