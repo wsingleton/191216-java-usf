b98fc2cb2a45f1a933918cab78d2cf0cf26e346a
Jeremiah Wolf <jeremiahthomaswolf@gmail.com>
2019-12-23 13:22:13 -0500

    adding core java assignment

--- Jeremiah_Wolf_Code/Core Java/Basic IO/Reading from a file/src/Task.java
+++ Jeremiah_Wolf_Code/Core Java/Basic IO/Reading from a file/src/Task.java
@@ -8,7 +8,39 @@ public class Task {
         // Instantiate an array
         String[] lines = new String[0];
 
-        // Provide your implementation here
+        //check if the provided file is valid, if not return an empty array.
+        if (quoteFile == null || !quoteFile.exists()) {
+            System.err.println("Invalid file provided.");
+            return lines;
+        }
+
+        // Use a BufferReader to traverse each line of the file
+        try {
+
+            BufferedReader fileReader = new BufferedReader(new FileReader(quoteFile));
+
+            // Obtain the first line of the file
+            String line = fileReader.readLine();
+
+            //Loop until there are no lines left in the file
+            while (line != null) {
+
+                //make a new array with an increased capacity of one index
+                lines = Arrays.copyOf(lines, lines.length +1);
+
+                // add line into array and procced to the next line of the file
+                lines[lines.length -1] = line;
+                line = fileReader.readLine();
+
+            }
+
+        }
+        //handle exceptions
+        catch (Exception e){
+            System.err.println("An unexpected exception occurred.");
+        }
+
+        return lines;
 
     }
 
--- Jeremiah_Wolf_Code/Core Java/Basic IO/Writing to a file/src/Task.java
+++ Jeremiah_Wolf_Code/Core Java/Basic IO/Writing to a file/src/Task.java
@@ -10,7 +10,32 @@ public class Task {
 
     public void writeToFile(String[] linesToWrite) {
 
-        // Provide your implementation here
+        //Verify that the arguments that are provided are valid for use
+        if (linesToWrite == null || linesToWrite.length ==0) {
+            return;
+        }
+
+        //wrap risky logic in a try/catch, and write the lines to the file
+        try{
+            //create the file which will be written to
+            File writeFile = new File ("resources/write.txt");
+            if (!writeFile.exists()) writeFile.createNewFile();
+
+            //define a filepath for the output
+            Path outPath = Paths.get("resources/write.txt");
+
+            //Convert the String into a List for use with the files methods
+            Arrays.asList(linesToWrite);
+            ArrayList<String> linesArray = new ArrayList<>();
+
+            //Write the lines to the file
+            Files.write(outPath, linesArray, Charset.defaultCharset());
+        }
+
+        catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("An unexpected exception occurred.");
+        }
 
     }
 
