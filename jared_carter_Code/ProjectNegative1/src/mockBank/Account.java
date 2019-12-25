b9c72f763e8846b82509586be31c2919358baeb0
unknown <jaredallencarterjac@gmail.com>
2019-12-25 18:08:01 -0500

    Bank demo practice

---
+++ jared_carter_Code/ProjectNegative1/src/mockBank/Account.java
@@ -0,0 +1,20 @@
+package mockBank;
+
+public class Account {
+
+    private double balance;
+    //setting balance to 0
+     public Account(){
+         balance =0.0;
+     }
+     //Deposit
+    public void deposit(double amount) {
+         balance = balance + amount;
+    }
+
+    //withdraw
+    public void withdraw(double amount) {
+         balance = balance - amount;
+    }
+
+}
