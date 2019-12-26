a05425e40cc86621e7c6fb471b6a04d74749da59
wsingleton <wezley.singleton@gmail.com>
2019-12-26 17:11:55 -0500

    pushing data structure and Comparable/Comparator demos

---
+++ Week_B/day4-demos/src/main/java/com/revature/util/Node.java
@@ -0,0 +1,28 @@
+package com.revature.util;
+
+public class Node<T> {
+
+    private T data;
+    private Node<T> nextNode;
+
+    public Node(T info, Node<T> next) {
+        this.data = info;
+        this.nextNode = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+}
