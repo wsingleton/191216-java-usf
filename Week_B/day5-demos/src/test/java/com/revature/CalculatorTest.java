4edc297752928ded25ee37bfd6be5c16ada33cbe
wsingleton <wezley.singleton@gmail.com>
2019-12-27 16:37:39 -0500

    pushing day 5 demos

---
+++ Week_B/day5-demos/src/test/java/com/revature/CalculatorTest.java
@@ -0,0 +1,47 @@
+package com.revature;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+
+public class CalculatorTest {
+
+    private Calculator sut;
+
+    @Before
+    public void setUp() {
+        sut = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        sut = null;
+    }
+
+    @Test
+    public void testMultiplyWithThreeAndFive() {
+        int expectedResult = 15;
+        int actualResult = sut.multiply(3, 5);
+        assertEquals("3 times 5 is 15", expectedResult, actualResult);
+    }
+
+    @Test
+    public void testMultiplyWithFiveAndThree() {
+        int expectedResult = 15;
+        int actualResult = sut.multiply(5, 3);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testMultiplyWithZeroAndFifteen() {
+        int expectedResult = 0;
+        int actualResult = sut.multiply(0, 15);
+        assertThat(actualResult, is(expectedResult));
+    }
+
+}
