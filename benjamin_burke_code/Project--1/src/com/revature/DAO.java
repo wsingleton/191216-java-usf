81401fb1b465d028f16d289895d0f24a435b28ec
Ben <benjaminstevenburke@gmail.com>
2019-12-28 18:00:38 -0500

    added text file and writer file for text

--- benjamin_burke_code/Project--1/src/com/revature/DAO.java
+++ benjamin_burke_code/Project--1/src/com/revature/DAO.java
@@ -1,4 +1,23 @@
 package com.revature;
 
+import com.revature.models.Account;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class DAO {
+
+    final static String fileLocation ="src/com/revature/Accounts.txt";
+
+    void addAccount(Account a){
+        //must write the account user to the text file
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileLocation, true))){
+            writer.write(a.toString());
+        } catch (IOException e){
+            e.printStackTrace();
+
+        }
+    }
+
 }
