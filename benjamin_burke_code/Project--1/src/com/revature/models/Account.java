156c3d50720331fa74b2fcebe9698cc18e1e54e5
Ben <benjaminstevenburke@gmail.com>
2019-12-28 17:53:36 -0500

    getters and setters created along with constructor

--- benjamin_burke_code/Project--1/src/com/revature/models/Account.java
+++ benjamin_burke_code/Project--1/src/com/revature/models/Account.java
@@ -1,4 +1,51 @@
 package com.revature.models;
 
 public class Account {
+
+    private String username;
+    private String password;
+    private double balance;
+
+    public Account(){
+
+    }
+
+    public Account(String username, String password, double balance) {
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
