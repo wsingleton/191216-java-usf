6f40a890d75efb4af0f927ac7bf6e7eaf7b4da54
Ben <benjaminstevenburke@gmail.com>
2019-12-28 17:51:08 -0500

    start method added

--- benjamin_burke_code/Project--1/src/com/revature/App.java
+++ benjamin_burke_code/Project--1/src/com/revature/App.java
@@ -1,4 +1,40 @@
 package com.revature;
 
+import java.util.Scanner;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        start();
+    }
+
+    static  void start(){
+        System.out.println("Welcome to the bank app!"
+                + "\nPlease log in"
+                + "\n1: Log In"
+                + "\n2: Register");
+
+        Scanner scan = new Scanner(System.in);
+        int op = 0;
+        try {
+            op=Integer.parseInt(scan.nextLine());
+
+        }
+        catch (NumberFormatException nfe){
+            System.out.println("Sorry...please pick another number!");
+            start();
+            scan.close();
+            return;
+        }
+        switch(op) {
+            case 1: logIn(); break;
+            case 2: signUp(); break;
+            default: System.out.println("Sorry, that's not an option. Please try again");
+                start();
+                scan.close();
+                return;
+        }
+        scan.close();
+    }
 }
