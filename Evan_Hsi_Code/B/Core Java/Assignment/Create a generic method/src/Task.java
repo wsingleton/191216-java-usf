b6e4462ce1a206bc54528d93daf9a7ae2f77c308
Evan Hsi <evanhsi@Evans-MacBook-Pro.local>
2019-12-31 00:36:43 -0500

    core

--- Evan_Hsi_Code/B/Core Java/Assignment/Create a generic method/src/Task.java
+++ Evan_Hsi_Code/B/Core Java/Assignment/Create a generic method/src/Task.java
@@ -1,7 +1,9 @@
 public class Task <T> {
 
     public void genericPrinter(T... a) {
-        for(T t:a) System.out.println(t);
+        for(T t:a) {
+            System.out.println(t);
+        }
     }
 
 }
