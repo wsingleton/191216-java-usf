a835a4939cd317a88941f8debd9c159eb98ae9db
Tannilen Reagan <tannilen.reagan@gmail.com>
2019-12-30 21:08:24 -0500

    21:00 push of Java Core - all tasks except Sort Employees

--- tannilen_reagan_code/WeekB/Core Java/Assignment/Word counter/src/Task.java
+++ tannilen_reagan_code/WeekB/Core Java/Assignment/Word counter/src/Task.java
@@ -1,23 +1,53 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Task {
 
     public String[] extractTextFromFile(File file) {
 
-        // Provide your implementation here
+        if (file==null) {
+            return new String[0];
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            line=reader.readLine();
+            /*
+             * Wezley, if you're going to ask us to create something that parses
+             * data out of a file, please tell us how that data is divided up, so
+             * we're not left guessing whether it's one word per line, a chain of
+             * words separated by spaces, a csv format, etc.  It would help a lot.
+             */
+            if (line==null || line==""){
+                return new String[0];
+            }
+            String[] extracted=line.split(" ");
+            return extracted;
+        }
+        catch (IOException ioe) {
+            return new String[0];
+        }
+
 
     }
 
     public Map<String, Integer> countWords(String[] extractedText) {
 
-        // Provide your implementation
+        if (extractedText==null || extractedText.length==0) {
+            Map<String,Integer> map=new HashMap<>();
+            return map;
+            }
+        else {
+            Map<String, Integer> wordCount=new HashMap<>();
+            for (String s:extractedText) {
+                if (wordCount.containsKey(s)) {
+                    wordCount.put(s,wordCount.get(s)+1);
+                }
+                else {
+                    wordCount.put(s, 1);
+                }
+            }
+            return wordCount;
+        }
 
     }
 
