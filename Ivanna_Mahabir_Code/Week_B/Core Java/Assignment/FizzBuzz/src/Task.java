36aebda76ff9a1039e6250aa37a6f1a8d4e3bc70
Ivanna Mahabir <iv.mahabir@gmail.com>
2020-01-03 08:58:26 -0500

    changes to Core Java edutools

--- Ivanna_Mahabir_Code/Week_B/Core Java/Assignment/FizzBuzz/src/Task.java
+++ Ivanna_Mahabir_Code/Week_B/Core Java/Assignment/FizzBuzz/src/Task.java
@@ -2,8 +2,20 @@ public class Task {
 
     public String[] fizzBuzz(int[] values) {
 
-        // Provide your implementation here
+        if(values == null || values.length == 0) {
+            return new String[0];
+        }
 
+
+        String[] args = new String[values.length];
+
+        for(int i = 0; i < values.length; i++){
+            if(((values[i] % 15) == 0) && ((values[i] % 5) == 0)) args[i] = "fizzbuzz";
+            else if((values[i] % 5) == 0 ) args[i] = "buzz";
+            else if((values[i] % 3) == 0 ) args[i] = "fizz";
+            else args[i] = String.valueOf(values[i]);
+        }
+    return args;
     }
 
 }
