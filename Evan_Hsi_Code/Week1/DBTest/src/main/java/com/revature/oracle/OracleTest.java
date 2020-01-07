58ee11f831e817c2934048c96d8724603f32f55f
Evan Hsi <evanhsi@Evans-MacBook-Pro.local>
2020-01-07 14:40:43 -0500

    SQL script table creation, jdbc connection to aws

--- Evan_Hsi_Code/Week1/DBTest/src/main/java/com/revature/oracle/OracleTest.java
+++ Evan_Hsi_Code/Week1/DBTest/src/main/java/com/revature/oracle/OracleTest.java
@@ -10,12 +10,22 @@ public class OracleTest {
         try {
             //String url = "jdbc:oracle:thin:@localhost:11521/~/Documents/docker-images/OracleDatabase/SingleInstance/dockerfiles/18.4.0/dockerfiles_oracle_1";
             //String url = "jdbc:oracle:thin:OT/orcl1234@xepdb1:11521:xe";
-            String url = "jdbc:oracle:thin:@localhost:11521:xe";
-            conn = DriverManager.getConnection(url, "sys as sysdba", "testing12345");
-            //conn = DriverManager.getConnection(url, "OT", "ORCL1234");
+            //String url = "jdbc:oracle:thin:sys as sysdba/testing12345@xepdb1:11521:xe";
+            //String url = "jdbc:oracle:thin:@localhost:11521:xe";
+            String url = "jdbc:oracle:thin:@revature.clnscuumfo9c.us-east-2.rds.amazonaws.com:1521:ORCL";
+            //String url = "jdbc:oracle:oci:OT/HELPME@localhost:11521:xe";
+            conn = DriverManager.getConnection(url, "admin", "deucalion13");
+            //conn = DriverManager.getConnection(url, "sys as sysdba", "testing12345");
+            //conn = DriverManager.getConnection(url, "OT@XEPDB1", "HELPME");
+            //conn = DriverManager.getConnection(url);
+
+            if(conn != null) {
+                System.out.println("Connected");
+            }
         }
         catch (SQLException sqle) { System.out.println(sqle.getMessage()); }
 
+        /*
         PreparedStatement ps;
             try {
                 ps = conn.prepareStatement("CREATE TABLE");
@@ -23,5 +33,6 @@ public class OracleTest {
             catch(SQLException sqle) {
                 System.out.println(sqle.getMessage());
             }
+         */
     }
 }
