d1eaf0047da75b314fe0e0efef7068cb47046882
wsingleton <wezley.singleton@gmail.com>
2020-01-09 17:03:54 -0500

    pushing last of revabooks jdbc demos

--- revabooks/src/main/java/com/revature/revabooks/TestDriver.java
+++ revabooks/src/main/java/com/revature/revabooks/TestDriver.java
@@ -2,8 +2,10 @@ package com.revature.revabooks;
 
 import com.revature.revabooks.models.Book;
 import com.revature.revabooks.models.User;
+import com.revature.revabooks.models.Wishlist;
 import com.revature.revabooks.repos.BookRepository;
 import com.revature.revabooks.repos.UserRepository;
+import com.revature.revabooks.repos.WishlistRepository;
 import com.revature.revabooks.util.ConnectionFactory;
 
 import java.sql.Connection;
@@ -13,9 +15,11 @@ public class TestDriver {
 
     public static void main(String[] args) {
 
-        BookRepository testRepo = new BookRepository();
-        Set<Book> didItWork = testRepo.findAll();
-        didItWork.forEach(book -> System.out.println(book));
+        WishlistRepository testRepo = new WishlistRepository();
+        Wishlist didItWork = testRepo.getWishlistByUserId(1);
+        System.out.println(didItWork);
+        System.out.println(didItWork.getOwner());
+        didItWork.getBookWishlist().forEach(book -> System.out.println(book));
 
     }
 
