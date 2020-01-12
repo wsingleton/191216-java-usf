d1483c76064541508f51b0b67352f2b34e1a44cf
wsingleton <wezley.singleton@gmail.com>
2020-01-12 01:11:05 -0500

    encapsulating application state

--- revabooks/src/main/java/com/revature/revabooks/util/AppState.java
+++ revabooks/src/main/java/com/revature/revabooks/util/AppState.java
@@ -1,4 +1,78 @@
 package com.revature.revabooks.util;
 
-public class StateStore {
+import com.revature.revabooks.repos.BookRepository;
+import com.revature.revabooks.repos.UserRepository;
+import com.revature.revabooks.repos.WishlistRepository;
+import com.revature.revabooks.screens.*;
+import com.revature.revabooks.services.BookService;
+import com.revature.revabooks.services.UserService;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class AppState {
+
+    private BufferedReader console;
+    private UserSession currentSession;
+    private ScreenRouter router;
+    private boolean appRunning;
+
+    public AppState() {
+
+        System.out.println("[LOG] - Initializing application...");
+
+        appRunning = true;
+        console = new BufferedReader(new InputStreamReader(System.in));
+
+        final UserRepository userRepo = new UserRepository();
+        final BookRepository bookRepo = new BookRepository();
+        final WishlistRepository wishlistRepo = new WishlistRepository();
+
+        final UserService userService = new UserService(userRepo);
+        final BookService bookService = new BookService(bookRepo, wishlistRepo);
+
+        router = new ScreenRouter();
+        router.addScreen(new HomeScreen())
+                .addScreen(new RegisterScreen(userService))
+                .addScreen(new LoginScreen(userService))
+                .addScreen(new DashboardScreen())
+                .addScreen(new UserProfileScreen(userService))
+                .addScreen(new SearchBooksScreen(bookService));
+
+        System.out.println("[LOG] - Application initialization complete.");
+
+    }
+
+    public BufferedReader getConsole() {
+        return console;
+    }
+
+    public ScreenRouter getRouter() {
+        return router;
+    }
+
+    public UserSession getCurrentSession() {
+        return currentSession;
+    }
+
+    public void setCurrentSession(UserSession currentSession) {
+        this.currentSession = currentSession;
+    }
+
+    public void invalidateCurrentSession() {
+        this.currentSession = null;
+    }
+
+    public boolean isSessionValid() {
+        return (this.currentSession != null);
+    }
+
+    public boolean isAppRunning() {
+        return appRunning;
+    }
+
+    public void setAppRunning(boolean appRunning) {
+        this.appRunning = appRunning;
+    }
+
 }
