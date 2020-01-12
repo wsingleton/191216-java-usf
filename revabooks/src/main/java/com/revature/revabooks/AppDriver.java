609b2bffb8d24d6e575bfbcce019e385d55280ba
wsingleton <wezley.singleton@gmail.com>
2020-01-12 01:10:43 -0500

    moving state to a store object (AppState)

--- revabooks/src/main/java/com/revature/revabooks/AppDriver.java
+++ revabooks/src/main/java/com/revature/revabooks/AppDriver.java
@@ -7,6 +7,7 @@ import com.revature.revabooks.repos.WishlistRepository;
 import com.revature.revabooks.screens.*;
 import com.revature.revabooks.services.BookService;
 import com.revature.revabooks.services.UserService;
+import com.revature.revabooks.util.AppState;
 import com.revature.revabooks.util.ScreenRouter;
 import com.revature.revabooks.util.UserSession;
 
@@ -15,40 +16,12 @@ import java.io.InputStreamReader;
 
 public class AppDriver {
 
-    public static BufferedReader console;
-    public static UserSession currentSession;
-    public static ScreenRouter router;
-    public static boolean appRunning;
-
-    static {
-        System.out.println("[LOG] - Initializing application...");
-
-        appRunning = true;
-        console = new BufferedReader(new InputStreamReader(System.in));
-
-        final UserRepository userRepo = new UserRepository();
-        final BookRepository bookRepo = new BookRepository();
-        final WishlistRepository wishlistRepo = new WishlistRepository();
-
-        final UserService userService = new UserService(userRepo);
-        final BookService bookService = new BookService(bookRepo, wishlistRepo);
-
-        router = new ScreenRouter();
-        router.addScreen(new HomeScreen())
-              .addScreen(new RegisterScreen(userService))
-              .addScreen(new LoginScreen(userService))
-              .addScreen(new DashboardScreen())
-              .addScreen(new UserProfileScreen(userService))
-              .addScreen(new SearchBooksScreen(bookService));
-
-        System.out.println("[LOG] - Application initialization complete.");
-
-    }
+    public static AppState app = new AppState();
 
     public static void main(String[] args) {
 
-        while (appRunning) {
-            router.navigate("/home");
+        while (app.isAppRunning()) {
+            app.getRouter().navigate("/home");
         }
 
     }
