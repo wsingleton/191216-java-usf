742637fcaee3c46b142ab22c37961ca8b9845da4
wsingleton <wezley.singleton@gmail.com>
2020-01-11 19:45:47 -0500

    refactor Dashboard to avoid dead paths, removed unnecessary inline comments

--- revabooks/src/main/java/com/revature/revabooks/screens/DashboardScreen.java
+++ revabooks/src/main/java/com/revature/revabooks/screens/DashboardScreen.java
@@ -13,59 +13,55 @@ public class DashboardScreen extends Screen {
     public void render() {
 
         String userSelection;
-
+        boolean loopMenu = true;
         System.out.println("Rendering " + currentSession.getSessionUser().getFirstName() + "'s Dashboard...");
 
-        // Display the standard user dashboard (will need to create admin version of dashboard)
-        System.out.println("\n\n+---------------------------------+\n");
-        System.out.println("1) Search Books");
-        System.out.println("2) My Favorite Books");
-        System.out.println("3) My Wish List");
-        System.out.println("4) Shopping Cart");
-        System.out.println("5) Edit Profile");
-        System.out.println("6) Sign Out");
+        while (loopMenu) {
 
-        try {
+            System.out.println("\n\n+---------------------------------+\n");
+            System.out.println("1) Search Books");
+            System.out.println("2) My Favorite Books");
+            System.out.println("3) My Wish List");
+            System.out.println("4) Shopping Cart");
+            System.out.println("5) Edit Profile");
+            System.out.println("6) Sign Out");
 
-            // Get the user's menu selection
-            System.out.print("Selection: ");
-            userSelection = console.readLine();
+            try {
 
-            // Navigate to the appropriate screen based on the user's selection
-            switch(userSelection) {
-                case "1":
-                    // Navigate to the SearchBooksScreen
-                    router.navigate("/search");
-                    break;
-                case "2":
-                    // Navigate to the FavoriteBooksScreen
-                    router.navigate("/favorites");
-                    break;
-                case "3":
-                    // Navigate to the WishlistScreen
-                    System.out.println("WishlistScreen under construction...");
-                    break;
-                case "4":
-                    // Navigate to the ShoppingCartScreen
-                    System.out.println("ShoppingCartScreen under construction...");
-                    break;
-                case "5":
-                    // Navigate to the UserProfileScreen
-                    router.navigate("/profile");
-                    break;
-                case "6":
-                    System.out.println(currentSession.getSessionUser().getUsername() + " signing out...");
-                    currentSession = null;
-                    break;
-                default:
-                    System.out.println("Invalid Selection!");
-                    router.navigate("/dashboard");
+                System.out.print("Selection: ");
+                userSelection = console.readLine();
+
+                switch (userSelection) {
+                    case "1":
+                        router.navigate("/search");
+                        break;
+                    case "2":
+                        System.err.println("FavoritesScreen under construction...");
+                        break;
+                    case "3":
+                        System.err.println("WishlistScreen under construction...");
+                        break;
+                    case "4":
+                        System.err.println("ShoppingCartScreen under construction...");
+                        break;
+                    case "5":
+                        router.navigate("/profile");
+                        break;
+                    case "6":
+                        System.out.println(currentSession.getSessionUser().getUsername() + " signing out...");
+                        loopMenu = false;
+                        currentSession = null;
+                        break;
+                    default:
+                        System.out.println("Invalid Selection!");
+                }
+
+            } catch (Exception e) {
+                System.err.println("[ERROR] - " + e.getMessage());
+                System.out.println("[LOG] - Shutting down application");
+                appRunning = false;
             }
 
-        } catch (Exception e) {
-            System.err.println("[ERROR] - " + e.getMessage());
-            System.out.println("[LOG] - Shutting down application");
-            appRunning = false;
         }
 
     }
