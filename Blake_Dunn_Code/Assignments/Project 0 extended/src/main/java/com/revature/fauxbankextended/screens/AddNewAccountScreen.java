106da825f9f2e4332282557a2db20cba915de0de
Blake Dunn <bdunn@DESKTOP-9VJVNCN>
2020-01-14 09:38:21 -0500

    commit add new account screen, allows users to add a second account

--- Blake_Dunn_Code/Assignments/Project 0 extended/src/main/java/com/revature/fauxbankextended/screens/AddNewAccountScreen.java
+++ Blake_Dunn_Code/Assignments/Project 0 extended/src/main/java/com/revature/fauxbankextended/screens/AddNewAccountScreen.java
@@ -1,13 +1,37 @@
 package com.revature.fauxbankextended.screens;
 
+import com.revature.fauxbankextended.services.UserService;
+
+import static com.revature.fauxbankextended.BankDriver.*;
+
 public class AddNewAccountScreen extends Screen {
 
-    public AddNewAccountScreen() {
+    private UserService userService;
+
+    public AddNewAccountScreen(UserService userService) {
         super("AddNewAccountScreen", "/addacct");
+        this.userService = userService;
     }
 
     @Override
     public void render() {
+
+        System.out.println("What type of account would you like to add?");
+        System.out.println("1) Checking");
+        System.out.println("2) Savings");
+
+        try {
+            System.out.print(">");
+            String choice = app().getConsole().readLine();
+            userService.setNewAccount(app().getCurrentSession().getSessionUser(), choice);
+
+            if (app().getCurrentSession() != null) {
+                app().getRouter().navigate("/dashboard");
+            }
+
+        }catch (Exception e) {
+            System.err.println("[ERROR] - " + e.getMessage());
+        }
 
     }
 }
