f3c23361aee6015759a94e921e2f2a4910b1a072
Niles Diggs <ndiggs@wesleyan.edu>
2020-01-14 16:57:38 -0500

    fixing more screen stuff

--- Project0_final/src/main/java/com/revature/BankMain.java
+++ Project0_final/src/main/java/com/revature/BankMain.java
@@ -1,6 +1,26 @@
 package com.revature;
 
+import com.revature.models.User;
+import com.revature.screens.AccountScreen;
+import com.revature.util.ScreenNavigation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class BankMain {
+
+    public static User currentUser;
+    public static ScreenNavigation navigation;
+    public static boolean appLaunched;
+    public static BufferedReader userInputs;
+
+    static {
+        appLaunched = true;
+        userInputs = new BufferedReader(new InputStreamReader(System.in));
+
+        navigation = new ScreenNavigation();
+        navigation.addScreen(new AccountScreen());
+    }
     public static void main(String[] args) {
 
     }
--- Project0_final/src/main/java/com/revature/screens/AccountScreen.java
+++ Project0_final/src/main/java/com/revature/screens/AccountScreen.java
@@ -1,4 +1,21 @@
 package com.revature.screens;
 
-public class AccountScreen {
+import static com.revature.BankMain.userInputs;
+
+public class AccountScreen extends Screen {
+
+    public AccountScreen() {
+        super("AccountScreen", "/account");
+    }
+
+    @Override
+    public void load() {
+        System.out.println("Your current balance is ");
+        System.out.println("Press 1) to make a deposit or 2) to make a withdraw");
+
+        try {
+
+            String path = userInputs.readLine();
+        }
+    }
 }
