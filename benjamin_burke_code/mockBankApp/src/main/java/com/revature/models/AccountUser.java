1d0285246e9c70f7223c4f2b7635e26c6b0575c3
Ben <benjaminstevenburke@gmail.com>
2020-01-16 04:06:55 -0500

    going back to what my original project was using the hash which i think would be easier for storing

--- benjamin_burke_code/mockBankApp/src/main/java/com/revature/models/AccountUser.java
+++ benjamin_burke_code/mockBankApp/src/main/java/com/revature/models/AccountUser.java
@@ -1,20 +1,60 @@
 package com.revature.models;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class AccountUser {
 
-    private int userId;
-    private int accountId;
+    private User customer;
+    private Set<Account> accountUser;
 
-    public AccountUser(int userId, int accountId) {
-        this.userId = userId;
-        this.accountId = accountId;
+    public AccountUser(){
+        super();
+        accountUser = new HashSet<>();
+
     }
 
-    public int getUserId() {
-        return userId;
+    public AccountUser(User customer, Set<Account> accountUser) {
+        this.customer = customer;
+        this.accountUser = accountUser;
     }
 
-    public int getAccountId() {
-        return accountId;
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public Set<Account> getAccountUser() {
+        return accountUser;
+    }
+
+    public void setAccountUser(Set<Account> accountUser) {
+        this.accountUser = accountUser;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountUser that = (AccountUser) o;
+        return Objects.equals(customer, that.customer) &&
+                Objects.equals(accountUser, that.accountUser);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customer, accountUser);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountUser{" +
+                "customer=" + customer +
+                ", accountUser=" + accountUser +
+                '}';
     }
 }
