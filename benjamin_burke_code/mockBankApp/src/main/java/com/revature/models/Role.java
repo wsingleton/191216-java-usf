07b47cc6c337d8033714587c07f33463830ee93c
Ben <benjaminstevenburke@gmail.com>
2020-01-16 03:59:50 -0500

    roles are set up

--- benjamin_burke_code/mockBankApp/src/main/java/com/revature/models/Role.java
+++ benjamin_burke_code/mockBankApp/src/main/java/com/revature/models/Role.java
@@ -1,4 +1,28 @@
 package com.revature.models;
 
 public enum Role {
+
+    ADMIN("Admin"), BASIC_MEMBER("Basic Membeer");
+
+    private String roleName;
+
+    Role(String roleName){this.roleName=roleName;}
+
+    public static Role getRoleById(int id){
+
+        Role role=null;
+        switch (id){
+            case 1:
+                role = Role.ADMIN; break;
+            case 2:
+                role=Role.BASIC_MEMBER; break;
+        }return role;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "roleName='" + roleName + '\'' +
+                '}';
+    }
 }
