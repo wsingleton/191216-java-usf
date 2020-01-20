f5087a165c5d8f581c449457b28a17ceb7284626
Andrew Spiteri <spiteria@gmail.com>
2020-01-20 15:09:49 -0500

    equivalent arrays solution

--- Andrew_Spiteri_Code/Assignments/JavaScript/js_assignment.js
+++ Andrew_Spiteri_Code/Assignments/JavaScript/js_assignment.js
@@ -150,14 +150,18 @@ function areSimilar(array1, array2){
     }
 }
 
-/* 12. Equivalent Arrays */
+/* 12. Equivalent Arrays */ //correct
 
 function equvArr(array1, array2){
     if(array1.length == array2.length){
         for(let i = 0; i < array1.length; i++){
             for(let j = 0; j < array2.length; j++){
-                if(array1[i] === array2[j]){
-                    array2.splice(i, 1);
+                console.log(array1[i] + ' ' + array2[j]);
+                if(array1[i] == array2[j]){
+                    console.log(array2);
+                    array2.splice(j, 1);
+                    j--;
+                    break;
                 }
             }
         }
