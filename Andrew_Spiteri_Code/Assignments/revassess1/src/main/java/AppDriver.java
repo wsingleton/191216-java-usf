83ce3c2e72f5a79ce21c809496a410ca55b0400d
Andrew Spiteri <spiteria@gmail.com>
2020-01-21 13:35:04 -0500

    tier 1 complete

---
+++ Andrew_Spiteri_Code/Assignments/revassess1/src/main/java/AppDriver.java
@@ -0,0 +1,6 @@
+public class AppDriver {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Andrew_Spiteri_Code/Assignments/revassess1/src/main/java/models/User.java
@@ -0,0 +1,104 @@
+package models;
+
+import java.util.Objects;
+
+public class User {
+    private Integer id;
+    private String firstName, lastName, username, password, role;
+
+    public User(Integer id, String firstName, String lastName, String username, String password, String role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Integer sum(Integer [] array){
+        Integer solution = 0;
+        for (Integer i:
+             array) {
+            solution += i;
+        }
+        return solution;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(username, user.username) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, username, password, role);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
