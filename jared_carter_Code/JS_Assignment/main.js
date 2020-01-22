5d859852c992dc65883cdffd4d3533a67b41810f
unknown <jaredallencarterjac@gmail.com>
2020-01-21 23:30:17 -0500

    js_assignmentp2

--- jared_carter_Code/JS_Assignment/main.js
+++ jared_carter_Code/JS_Assignment/main.js
@@ -1,5 +1,5 @@
 
-
+//1
 window.onload = function(){
  let googleLink = document.getElementsByName('google')
  googleLink.forEach(element => {
@@ -25,3 +25,50 @@ element.setAttribute('href', 'https://twitter.com')
 
 })
 }
+//2
+let disablePlutoOption = document.getElementById('planet');
+let disablePlanet = disablePlutoOption.children;
+disablePlanet[3].setAttribute('disabled', true);
+//3
+document.getElementById("planet").addEventListener("change",alienText);
+//4
+//5
+//6
+
+function concatAllSpan(){
+
+    let span = document.getElementsByTagName("span")
+    spans = " "
+    for(let i = 0; i < span.length; i++){
+        spans += span [i].innerHTML;
+    }
+
+}
+
+
+
+//7
+
+
+document.getElementById("earth_time_check").addEventListener("click", displayEarthTime);
+
+function displayEarthTime(){
+var earthDate = new Date();
+var currentTime = date.toLocaleTimeString
+
+document.getElementById("earth_time").innerHTML = dt.toLocaleTimeString();
+}
+//8
+
+//9
+
+
+
+//10
+
+function walkTheDom(node, func) {
+    var children = node.childNodes;
+    for (var i = 0; i < children.length; i++) 
+        walk(children[i], func);
+    func(node);
+ }
