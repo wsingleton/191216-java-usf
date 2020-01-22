6ecf07f3bb9bcfba049980439cb6512047a43973
Andrew Spiteri <spiteria@gmail.com>
2020-01-22 09:50:16 -0500

    reimb status enum redone

--- Andrew_Spiteri_Code/Assignments/mock_ers/src/main/java/com/revature/mockERS/models/ERS_Reimbursement_Status.java
+++ Andrew_Spiteri_Code/Assignments/mock_ers/src/main/java/com/revature/mockERS/models/ERS_Reimbursement_Status.java
@@ -1,18 +1,33 @@
 package com.revature.mockERS.models;
 
 public enum ERS_Reimbursement_Status {
-    ACCEPTED("Accepted"), REJECTED("Rejected"), ON_HOLD("On Hold"), PRESPROCESSING("Pre-Processing");
+    ACCEPTED("Accepted"), REJECTED("Rejected"), PROCESSING("Processing"), PRESPROCESSING("Pre-Processing");
     private String status;
 
     ERS_Reimbursement_Status(String status){
         this.status = status;
     }
 
-    public String getStatus() {
+    public static ERS_Reimbursement_Status getStatusById(Integer id){
+        ERS_Reimbursement_Status status = null;
+        switch (id){
+            case 1:
+                status = ERS_Reimbursement_Status.PRESPROCESSING;
+                break;
+            case 2:
+                status = ERS_Reimbursement_Status.PROCESSING;
+                break;
+            case 3:
+                status = ERS_Reimbursement_Status.REJECTED;
+                break;
+            case 4:
+                status = ERS_Reimbursement_Status.ACCEPTED;
+        }
         return status;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    @Override
+    public String toString() {
+        return  status;
     }
 }
