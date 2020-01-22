fe8aaa133e4435ee4a7b2277f0ea35ec6bbb3d99
Andrew Spiteri <spiteria@gmail.com>
2020-01-22 09:33:54 -0500

    completed reimbursement_status enum

--- Andrew_Spiteri_Code/Assignments/mock_ers/src/main/java/com/revature/mockERS/models/ERS_Reimbursement_Status.java
+++ Andrew_Spiteri_Code/Assignments/mock_ers/src/main/java/com/revature/mockERS/models/ERS_Reimbursement_Status.java
@@ -1,5 +1,18 @@
 package com.revature.mockERS.models;
 
 public enum ERS_Reimbursement_Status {
-    ACCEPTED, REJECTED, ON_HOLD, WAITING
+    ACCEPTED("Accepted"), REJECTED("Rejected"), ON_HOLD("On Hold"), PRESPROCESSING("Pre-Processing");
+    private String status;
+
+    ERS_Reimbursement_Status(String status){
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
