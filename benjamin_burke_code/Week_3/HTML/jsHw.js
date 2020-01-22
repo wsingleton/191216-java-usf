bb6f4206c877f79f948ad727262ec76d3b67b8a8
Ben <benjaminstevenburke@gmail.com>
2020-01-22 08:25:02 -0500

    trying to update the part 1 at least

--- benjamin_burke_code/Week_3/HTML/jsHw.js
+++ benjamin_burke_code/Week_3/HTML/jsHw.js
@@ -15,7 +15,7 @@ console.log(leapyear(2016));
 
 
 //2. Create a function that checks for a valid email format.
-
+//
 //3. Write a JavaScript function to remove a character at the specified position of a given string and return the new string.
 
 //can probably use slice method....
@@ -49,16 +49,49 @@ const countVowels=(str)=>{
 console.log(countVowels("banana"));
 
 // Write a JavaScript function to reverse the elements of a given array.
+let arr=[]
+let revArr = arr.reverse();
 
 // Write  JavaScript function that adds the values of arrays, even if the arrays are different sizes. 
 // 	addArr([1,2,3],[3,2,1]) = [4,4,4]
 // 	addArr([1,2,3,4,5],[1,2,3]) = [2,4,6,4,5]
 
+let addArr=(arr1, arr2)=>{
+    newArr = arr1.concat(arr2);
+}
+
+
 // Write a JavaScript to find the longest string from a given array of strings and returns the string’s array index.
+const longestStr =(arr)=>{
+    let maxStr = arr[0].length
+    arr.map(a=>maxStr = Math.max(max, a.length));
+    result = arr.filter(a=> a.length == maxStr)
+}
+
 
 // Write a JavaScript function which accepts three integers representing side lengths of a triangle. The function should validate that the integers make a geometrically accurate. If the three integers could not make a triangle, return undefined. Otherwise calculate and return the area of the triangle.
+//this is so off
+findTriangle=(a, b, c)=>{
+    let parameter = (a +b+c)/2;
+    if(a+b>c){
+        let area = Math.sqrt(parameter*(parameter-a)*(parameter-b)*(parameter-c));
 
+        return area
+    } else 
+    return undefined;
+
+}
 // Write a JavaScript function which accepts two arrays, representing the side lengths of polygons. Return true if the polygons are similar, false if they are not.
+//do we need to account for the polygon sides or just for the arrays?
+polygon=(arr1, arr2)=>{
+    if(arr1.length === arr2.length){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+
 
 // Check if two arrays have the same size and contents (including if content order is not the same).
 // 	equivArr([4,2,8,4,7],[7,4,4,2,8]) = true
@@ -78,6 +111,7 @@ console.log(countVowels("banana"));
 
 // tictactoe([[empty, “O”, “X”],[“X”, “O”, empty],[empty, “O”, “X”]]) 
 // Prints “O is the winner”
+
 
 // 		tictactoe([[“X”, empty, “X”],[“O”, empty, empty],[empty, “O”, empty]]) 
 // Prints “No winner yet”
