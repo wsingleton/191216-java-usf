d29ed6944181f19b52800628a35cbe157de1809e
Andrew Spiteri <spiteria@gmail.com>
2020-01-22 09:28:12 -0500

    AppState functionality for UserSession

--- Andrew_Spiteri_Code/Assignments/mock_ers/src/main/java/com/revature/mockERS/util/AppState.java
+++ Andrew_Spiteri_Code/Assignments/mock_ers/src/main/java/com/revature/mockERS/util/AppState.java
@@ -1,5 +1,41 @@
 package com.revature.mockERS.util;
 
-public class AppState {
+import com.revature.mockERS.repositories.UserRepository;
 
+public class AppState {
+    private UserSession currentSession;
+    private Boolean appRunning;
+
+    public AppState(){
+        appRunning = true;
+
+        final UserRepository userRepo = new UserRepository();
+
+    }
+
+    public UserSession getCurrentSession() {
+        return currentSession;
+    }
+
+    public void setCurrentSession(UserSession currentSession) {
+        this.currentSession = currentSession;
+    }
+
+    public void invalidateCurrentSession(){
+        this.currentSession = null;
+    }
+
+    public Boolean isSessionValid(){
+        return (this.currentSession != null);
+    }
+
+    public void setAppRunning(Boolean appRunning) {
+        this.appRunning = appRunning;
+    }
+
+    public Boolean isAppRunning() {
+        return appRunning;
+    }
+
+    
 }
