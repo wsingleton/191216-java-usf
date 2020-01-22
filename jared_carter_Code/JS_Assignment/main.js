4f7d2f8d840c9656373b6487949d765b4fa7ba90
unknown <jaredallencarterjac@gmail.com>
2020-01-21 22:31:27 -0500

    jspart2.1

--- jared_carter_Code/JS_Assignment/main.js
+++ jared_carter_Code/JS_Assignment/main.js
@@ -1,16 +1,27 @@
-function triangleCheck(numberx,numbery, numberz) {
-
-    if ((numberxnumbery > numberz) && (numberxnumberz > numbery) && (numberynumberz > numberx)) {
-
-        var pre = (numberx + numbery + numberz)/2
-        return Math.sqrt(pre*((numberx)*(numbery)*(numberz)));;
-
-	}
-	else {
-		alert("Undefined.");
-	}
-  }
 
 
+window.onload = function(){
+ let googleLink = document.getElementsByName('google')
+ googleLink.forEach(element => {
+     element.setAttribute('href', 'https://www.google.com/')
+     element.innerText = 'GOOGLE'
+ })
+let twitterLink = document.getElementsByName('twitter')
+ twitterLink.forEach(element => {
+element.setAttribute('href', 'https://twitter.com')
+  element.innerText = 'TWITTER'
+ })
 
-    
+  let slackLink = document.getElementsByName('slack')
+  slackLink.forEach(element => {  element.setAttribute('href', 'https://slack.com/')
+      element.innerText = 'SLACK'
+  })
+
+
+ javadocsLink = document.getElementsByName('javadocs')
+      javadocsLink.forEach(element => {
+          element.setAttribute('href', 'https://docs.oracle.com/')
+          element.innerText = 'JAVADOCS'
+
+})
+}
