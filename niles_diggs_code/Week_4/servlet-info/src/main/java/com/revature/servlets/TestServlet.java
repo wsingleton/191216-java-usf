2be744b2a9370a3449401671ef6cdd5a75ea9b04
Niles Diggs <ndiggs@wesleyan.edu>
2020-01-23 11:20:07 -0500

    Week 4 day-4 morning notes

--- niles_diggs_code/Week_4/servlet-info/src/main/java/com/revature/servlets/TestServlet.java
+++ niles_diggs_code/Week_4/servlet-info/src/main/java/com/revature/servlets/TestServlet.java
@@ -1,15 +1,35 @@
 package com.revature.servlets;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class TestServlet extends HttpServlet {
 
     @Override
+    public void init(ServletConfig config) {
+        System.out.println("TestServlet.init() invoked!");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("TestServlet.destroy() invoked!");
+    }
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-        resp.getWriter().write("<h1>/test works!</h1>");
+        System.out.println("TestServlet.doGet() invoked!");
+
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>/test works!</h!>");
+
+        resp.addHeader("Authorization", "randomm-value");
+        resp.setStatus(200);
+        resp.setContentType("text/html");
+        
     }
 }
