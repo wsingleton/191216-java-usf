e85771e39d768ea9f8fc8ea2dab81f37b44ade7b
Dayvon Allen <dayvon@USF-Gold-Wifi-226-73-194.laptops.usf.edu>
2020-01-23 09:03:23 -0500

    servlet stuff

---
+++ Dayvon_Allen_code/Week_4/servlet-intro/src/main/java/com/revature/servlet/TestServlet.java
@@ -0,0 +1,17 @@
+package com.revature.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+
+        res.getWriter().write("<h1>/Test works!</h1>");
+
+    }
+}
