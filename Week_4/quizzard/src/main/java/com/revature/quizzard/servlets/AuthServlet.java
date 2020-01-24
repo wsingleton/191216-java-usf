96a93e285b4a9df1bf37b611208214121f21d2e5
Wezley Singleton <wezley.singleton@gmail.com>
2020-01-24 12:01:28 -0500

    pushing up rest of servlet demo

--- Week_4/quizzard/src/main/java/com/revature/quizzard/servlets/AuthServlet.java
+++ Week_4/quizzard/src/main/java/com/revature/quizzard/servlets/AuthServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -20,6 +21,13 @@ import java.io.PrintWriter;
 public class AuthServlet extends HttpServlet {
 
     public final UserService userService = new UserService(new UserRepository());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getSession(false) != null) {
+            req.getSession().invalidate();
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -34,6 +42,8 @@ public class AuthServlet extends HttpServlet {
             User authUser = userService.authenticate(creds.getUsername(), creds.getPassword());
             String authUserJSON = mapper.writeValueAsString(authUser);
             writer.write(authUserJSON);
+            HttpSession session = req.getSession();
+            session.setAttribute("this-user", authUser);
 
         } catch (MismatchedInputException e) {
             resp.setStatus(400);
---
+++ Week_4/quizzard/src/main/webapp/js/main.js
@@ -0,0 +1,65 @@
+window.onload = () => {
+    console.log('did the JS load?');
+    loadLogin();
+    document.getElementById('logout').addEventListener('click', logout);
+}
+
+function loadLogin() {
+
+    console.log('in loadLogin()');
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'login.view', true);
+    xhr.send();
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('root').innerHTML = xhr.responseText;
+            document.getElementById('login').addEventListener('click', login);
+        }
+    }
+
+}
+
+function login() {
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    let creds = {
+        username: username,
+        password: password
+    };
+
+    let credJSON = JSON.stringify(creds);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'auth', true);
+    xhr.send(credJSON);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+
+                let user = JSON.parse(xhr.responseText);
+                console.log(user);
+                // loadDashboard();
+
+            }
+
+            if (xhr.status === 401) {
+                document.getElementById('login-message').innerText = 'Login failed!';
+            }
+        }
+    }
+
+}
+
+function logout() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'auth', true);
+    xhr.send();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log('logout successful!')
+        }
+    }
+}
