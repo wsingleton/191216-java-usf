c83ec0364b3c677d8027a644a2cf2cfa74030386
Andrew Spiteri <spiteria@gmail.com>
2020-01-25 15:00:31 -0500

    testing RequestViewHelper

--- Andrew_Spiteri_Code/Assignments/mock_ers/src/main/java/com/revature/mockERS/MockERSDriver.java
+++ Andrew_Spiteri_Code/Assignments/mock_ers/src/main/java/com/revature/mockERS/MockERSDriver.java
@@ -1,7 +1,10 @@
 package com.revature.mockERS;
 
+import com.revature.mockERS.util.RequestViewHelper;
+
 public class MockERSDriver {
     public static void main(String[] args) {
-
+        RequestViewHelper rvh = new RequestViewHelper();
+        rvh.process("/ers-app/dashboard.view");
     }
 }
--- Andrew_Spiteri_Code/Assignments/mock_ers/src/main/java/com/revature/mockERS/util/RequestViewHelper.java
+++ Andrew_Spiteri_Code/Assignments/mock_ers/src/main/java/com/revature/mockERS/util/RequestViewHelper.java
@@ -10,9 +10,12 @@ public class RequestViewHelper {
             case "/ers-app/register.view":
                 return "partials/register.html";
             case "/ers-app/dashboard.view":
+                System.out.println("Line 1");
                 if(UserSession.isAdminOrManager()){
+                    System.out.println("Line 2");
                     return "partials/admin-dashboard.html";
                 }else{
+                    System.out.println("Line 3");
                     return "partials/basic-dashboard.html";
                 }
             default:
