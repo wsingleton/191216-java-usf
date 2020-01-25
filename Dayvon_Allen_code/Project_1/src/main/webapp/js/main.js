c4928910cae56c479ca9a7c58a11bb40846ed7bc
Dayvon Allen <dayvon@107-144-156-227.biz.spectrum.com>
2020-01-25 10:39:50 -0500

    did stuff

--- Dayvon_Allen_code/Project_1/src/main/webapp/js/main.js
+++ Dayvon_Allen_code/Project_1/src/main/webapp/js/main.js
@@ -3,6 +3,14 @@ loadHome()
 }
 function loadLogin() {
     console.log('in loadLogin()');
+    if(document.querySelectorAll("head")[0].children.length === 7){
+        document.querySelectorAll("head")[0].children[6].remove();
+    }
+     let css = document.createElement('link');
+     css.setAttribute('rel', 'stylesheet');
+     css.setAttribute('href', 'css/login.css');
+     document.getElementsByTagName("head")[0].appendChild(css);
+     console.log(document.querySelectorAll("head")[0].children);
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'login.view', true);
     xhr.send();
@@ -31,8 +39,9 @@ function loadLogin() {
             password.addEventListener('focus', () => {
                 password.classList.add('focus');
             })
+            document.getElementById('sign-up').addEventListener('click', loadRegister);
+            document.getElementById('login').addEventListener('click', login);
 
-            document.getElementById("login").addEventListener('click', login);
         }
     }
 }
@@ -92,6 +101,91 @@ function loadDashboard() {
                     }
 }
 
+function loadRegister() {
+ if(document.querySelectorAll("head")[0].children.length === 7){
+        document.querySelectorAll("head")[0].children[6].remove();
+    }
+let css = document.createElement('link');
+     css.setAttribute('rel', 'stylesheet');
+     css.setAttribute('href', 'css/register.css');
+     document.getElementsByTagName("head")[0].appendChild(css);
+ let xhr = new XMLHttpRequest();
+
+                    xhr.open('GET', 'register.view', true);
+                    xhr.send();
+                    xhr.onreadystatechange = () => {
+                        if(xhr.readyState === 4 && xhr.status === 200) {
+                            document.getElementById("root").innerHTML = xhr.responseText;
+
+                            let firstname = document.querySelectorAll(".txtb input")[0];
+                            let lastname = document.querySelectorAll(".txtb input")[1];
+                            let username = document.querySelectorAll(".txtb input")[2];
+                            let email = document.querySelectorAll(".txtb input")[3];
+                            let password = document.querySelectorAll(".txtb input")[4];
+                            window.onload = () => {
+                                console.log(window.innerHeight);
+                                console.log(window.innerWidth);
+                            }
+                            firstname.addEventListener('focus', () => {
+                                let css = document.createElement('link');
+                                css.setAttribute('rel', 'stylesheet');
+                                css.setAttribute('href', 'login.css');
+                                document.getElementsByTagName("head")[0].appendChild(css)
+                                console.log(document.querySelectorAll("head")[0].children[5]);
+                                firstname.classList.add('focus');
+                            });
+
+                            firstname.addEventListener('blur', () => {
+                                if (firstname.value.length < 1) {
+
+                                    firstname.classList.remove('focus');
+                                }
+                            });
+
+                            lastname.addEventListener('blur', () => {
+                                if (lastname.value.length < 1) {
+                                    lastname.classList.remove('focus');
+                                }
+                            });
+
+                            lastname.addEventListener('focus', () => {
+                                lastname.classList.add('focus');
+                            });
+
+                            username.addEventListener('blur', () => {
+                                if (username.value.length < 1) {
+                                    username.classList.remove('focus');
+                                }
+                            });
+
+                            username.addEventListener('focus', () => {
+                                username.classList.add('focus');
+                            });
+
+                            email.addEventListener('blur', () => {
+                                if (email.value.length < 1) {
+                                    email.classList.remove('focus');
+                                }
+                            });
+
+                            email.addEventListener('focus', () => {
+                                email.classList.add('focus');
+                            });
+                            password.addEventListener('blur', () => {
+                                if (password.value.length < 1) {
+                                    password.classList.remove('focus');
+                                }
+                            });
+
+                            password.addEventListener('focus', () => {
+                                password.classList.add('focus');
+                            });
+
+                            document.getElementById("signIn").addEventListener("click", loadLogin);
+                        }
+                    }
+}
+
 function loadHome() {
 
  let xhr = new XMLHttpRequest();
@@ -101,6 +195,7 @@ function loadHome() {
                         if(xhr.readyState === 4 && xhr.status === 200) {
                             document.getElementById("root").innerHTML = xhr.responseText;
                             document.getElementById("loginBut").addEventListener("click", loadLogin);
+                            document.getElementById("registerBut").addEventListener("click", loadRegister)
                         }
                     }
 }
