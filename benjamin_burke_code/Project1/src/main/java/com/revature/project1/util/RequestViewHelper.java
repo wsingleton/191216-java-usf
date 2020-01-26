aaff67b6fd7aa7d5c194ab38c44eb0f45f945dc0
Ben <benjaminstevenburke@gmail.com>
2020-01-26 18:10:02 -0500

    js cant be commented out

--- benjamin_burke_code/Project1/src/main/java/com/revature/project1/util/RequestViewHelper.java
+++ benjamin_burke_code/Project1/src/main/java/com/revature/project1/util/RequestViewHelper.java
@@ -2,13 +2,21 @@ package com.revature.project1.util;
 
 public class RequestViewHelper {
 
-    public static String process(String uri){
+    public static String process(String uri) {
 
-        switch (uri){
+        switch (uri) {
             case "/Project1/home.view":
                 return "partials/home.html";
-            case "/project1/login.view":
+            case "/Project1/login.view":
                 return "partials/login.html";
+            case "/Project1/register.view":
+                return "partials/register.html";
+            case "/Project1/dashboard.view":
+                return "partials/dashboard.html";
+            case "/Project1/reimbs.view":
+                return "partials/reimb-table";
+            case "/Project1/newreimb.view":
+                return "Project1/newreimb.html";
             default:
                 return "";
         }
--- benjamin_burke_code/Project1/src/main/webapp/js/main.js
+++ benjamin_burke_code/Project1/src/main/webapp/js/main.js
@@ -1,5 +1,8 @@
+
 window.onload = () => {
-loadHome();
+    console.log('homescreen loaded?');
+    loadHome();
+
 
 }
 
@@ -12,74 +15,55 @@ function loadHome() {
     xhr.send();
     xhr.onreadystatechange =() => {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById("root").innerHTML = xhr.responseText;
+            document.getElementById('root').innerHTML = xhr.responseText;
             document.getElementById('home-login').addEventListener('click', loadLogin);
+            document.getElementById('home-register').addEventListener('click', loadRegister);
         }
     }
 }
-
-function loadDashboard() {
-
-    console.log('in dashboard');
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'dashboard.view', true);
-    xhr.send();
-    xhr.onreadystatechange =() => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById("root").innerHTML = xhr.responseText;
-
-        }
-    }
-}
-
-//function loadNav(){
-//
-//let xhr = new XMLHttpRequest();
-//    xhr.open('GET', 'nav.view', true);
-//    xhr.send();
-//    xhr.onreadystatechange =() => {
-//        if (xhr.readyState === 4 && xhr.status === 200) {
-//            document.getElementById("nav").innerHTML = xhr.responseText;
-//
-//        }
-//    }
-//}
-
-
 
 function loadLogin() {
-    console.log('in loadLogin()');
+
+    console.log('in loadLogin');
+
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'login.view', true);
     xhr.send();
     xhr.onreadystatechange = () => {
-        if(xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById("root").innerHTML = xhr.responseText;
-            document.getElementById('login').addEventListener('click', login);
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('root').innerHTML = xhr.responseText;
+            document.getElementById('login').addEventListener('click', function(event){
+                event.preventDefault();
+                login();
+            });
+
         }
     }
-
 }
 
 function login() {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
+
     let creds = {
         username: username,
         password: password
     };
+
     let credJSON = JSON.stringify(creds);
+
     let xhr = new XMLHttpRequest();
     xhr.open('POST', 'auth', true);
     xhr.send(credJSON);
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
+
                 let user = JSON.parse(xhr.responseText);
                 console.log(user);
-                // loadDashboard();
+                loadDashboard();
             }
+
             if (xhr.status === 401) {
                 document.getElementById('login-message').innerText = 'Login failed!';
             }
@@ -87,13 +71,84 @@ function login() {
     }
 }
 
+function loadRegister() {
+
+    console.log('in loadRegister()');
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'register.view', true);
+    xhr.send();
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('root').innerHTML = xhr.responseText;
+            document.getElementById('register').addEventListener('click', register);
+        }
+    }
+}
+
+function register() {
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let firstName = document.getElementById('firstname').value;
+    let lastName = document.getElementById('lastname').value;
+    let email = document.getElementById('email').value;
+
+    let creds = {
+        username: username,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+        email: email
+    };
+
+    let credJSON = JSON.stringify(creds);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'users', true);
+    xhr.send(credJSON);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+
+                let user = JSON.parse(xhr.responseText);
+                console.log(user);
+                loadDashboard();
+            }
+
+            if (xhr.status === 401) {
+                document.getElementById('register-message').innerText = 'Username is taken';
+            }
+        }
+    }
+}
+
+
+
 function logout() {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'auth', true);
     xhr.send();
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log('logout successful!')
+            console.log('Logout successful!');
+        }
+    }
+
+    loadHome();
+}
+
+function loadDashboard() {
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'dashboard.view', true);
+    xhr.send();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('root').innerHTML = xhr.responseText;
+            document.getElementById('newreimbbutton').addEventListener('click', loadNewReimb);
+            document.getElementById('viewreimbbutton').addEventListener('click', loadReimbs);
         }
     }
 }
+
