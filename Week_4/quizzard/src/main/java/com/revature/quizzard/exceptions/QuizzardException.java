715c3eff9445f3a40a118f349f1ab87816b3b22a
Wezley Singleton <wezley.singleton@gmail.com>
2020-01-30 15:23:12 -0500

    added a high-level wrapper exception

--- Week_4/quizzard/src/main/java/com/revature/quizzard/exceptions/QuizzardException.java
+++ Week_4/quizzard/src/main/java/com/revature/quizzard/exceptions/QuizzardException.java
@@ -1,4 +1,9 @@
 package com.revature.quizzard.exceptions;
 
-public class QuizzardException {
+public class QuizzardException extends RuntimeException {
+
+    public QuizzardException(Throwable e) {
+        super("An unspecified exception was thrown, see logs for more information", e);
+    }
+
 }
