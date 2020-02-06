fe18f63ac40f41dd50856451a6bbecc4763bd60e
Tannilen Reagan <tannilen.reagan@gmail.com>
2020-02-06 11:05:56 -0500

    Session factory underway

--- tannilen_reagan_code/Week6/HybernateDemo/src/main/java/com/revature/util/HibernateUtil.java
+++ tannilen_reagan_code/Week6/HybernateDemo/src/main/java/com/revature/util/HibernateUtil.java
@@ -1,4 +1,18 @@
 package com.revature.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration config=new Configuration();
+            config.configure();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
+        }
+    }
 }
