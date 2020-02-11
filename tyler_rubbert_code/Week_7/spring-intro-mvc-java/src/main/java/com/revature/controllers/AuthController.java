7a748df556818e7663a71a708b6ec97efdea356c
tylerrubbert <rubbert_12@live.com>
2020-02-11 16:28:16 -0500

    Spring magic

---
+++ tyler_rubbert_code/Week_7/spring-intro-mvc-java/src/main/java/com/revature/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package com.revature.controllers;
+
+import com.revature.dto.Credentials;
+import com.revature.dto.ErrorResponse;
+import com.revature.entities.AppUser;
+import com.revature.exceptions.AuthenticationException;
+import com.revature.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(consumes= MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+    public AppUser getUserByAuth(@RequestBody Credentials userCreds) {
+        return userService.authenticate(userCreds);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleFailedAuthentication(AuthenticationException e) {
+        ErrorResponse err = new ErrorResponse();
+        err.setMessage(e.getMessage());
+        err.setStatus(401);
+        err.setTimestamp(System.currentTimeMillis());
+        return err;
+    }
+
+}
---
+++ tyler_rubbert_code/Week_7/spring-intro-mvc-java/src/main/java/com/revature/exceptions/BadRequestException.java
@@ -0,0 +1,9 @@
+package com.revature.exceptions;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException() {
+        super("Invalid request made!");
+    }
+
+}
