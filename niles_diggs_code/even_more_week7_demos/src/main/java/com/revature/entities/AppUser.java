cafe456b62b7e0968a08d24ad6ae70466b4b879e
Niles Diggs <ndiggs@wesleyan.edu>
2020-02-12 14:04:48 -0500

    day3 morning projects completed

--- niles_diggs_code/even_more_week7_demos/src/main/java/com/revature/entities/AppUser.java
+++ niles_diggs_code/even_more_week7_demos/src/main/java/com/revature/entities/AppUser.java
@@ -1,5 +1,7 @@
 package com.revature.entities;
 
+import java.util.Objects;
+
 public class AppUser {
 
     private int id;
@@ -7,4 +9,78 @@ public class AppUser {
     private String password;
     private String username;
 
+    public AppUser() {
+        super();
+    }
+
+    public AppUser(String email, String password, String username) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+    public AppUser(int id, String email, String password, String username) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUser appUser = (AppUser) o;
+        return id == appUser.id &&
+                Objects.equals(email, appUser.email) &&
+                Objects.equals(password, appUser.password) &&
+                Objects.equals(username, appUser.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password, username);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
 }
--- niles_diggs_code/even_more_week7_demos/src/main/java/com/revature/entities/UserRole.java
+++ niles_diggs_code/even_more_week7_demos/src/main/java/com/revature/entities/UserRole.java
@@ -3,4 +3,14 @@ package com.revature.entities;
 public enum UserRole {
     ADMIN("Admin"), DEV("Dev"), BASIC_USER("Basci_user"), LOCKED("Locked");
 
+    private String roleName;
+
+    UserRole(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return this.roleName;
+    }
 }
