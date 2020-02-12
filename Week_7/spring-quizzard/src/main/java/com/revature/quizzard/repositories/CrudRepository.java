a0db96f5dded40057d77f27f8338bedfc7e1dfac
wsingleton <wezley.singleton@gmail.com>
2020-02-12 14:27:14 -0500

    added crudrepo method stubs

--- Week_7/spring-quizzard/src/main/java/com/revature/quizzard/repositories/CrudRepository.java
+++ Week_7/spring-quizzard/src/main/java/com/revature/quizzard/repositories/CrudRepository.java
@@ -1,4 +1,13 @@
 package com.revature.quizzard.repositories;
 
-public interface CrudRepository {
+import java.util.Set;
+
+public interface CrudRepository<T> {
+
+    Set<T> findAll();
+    T findById(int id);
+    T save(T newObj);
+    boolean update(T updatedObj);
+    boolean deleteById(int id);
+
 }
