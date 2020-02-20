321fb18e49973be4e9f269efaa58b8f232462e29
wsingleton <wezley.singleton@gmail.com>
2020-02-20 14:55:52 -0500

    added basic hystrix demo (sans dashboard)

--- Week_8/flash-card-service/src/main/java/com/revature/AppConfig.java
+++ Week_8/flash-card-service/src/main/java/com/revature/AppConfig.java
@@ -3,8 +3,10 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableHystrix
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
--- Week_8/flash-card-service/src/main/java/com/revature/controllers/FeignController.java
+++ Week_8/flash-card-service/src/main/java/com/revature/controllers/FeignController.java
@@ -1,5 +1,6 @@
 package com.revature.controllers;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.revature.intercom.UserClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -19,14 +20,24 @@ public class FeignController {
         this.userClient = client;
     }
 
+    @HystrixCommand(fallbackMethod = "fallback1")
     @GetMapping(value = "/intercom1", produces = MediaType.TEXT_PLAIN_VALUE)
     public String intercom1() {
         return userClient.test();
     }
 
+    @HystrixCommand(fallbackMethod = "fallback2")
     @GetMapping(value = "/intercom2", produces = MediaType.TEXT_PLAIN_VALUE)
     public String intercom2() {
         return userClient.getColorHex();
+    }
+
+    public String fallback1() {
+        return "Fallback method #1 was invoked!";
+    }
+
+    public String fallback2() {
+        return "Fallback method #2 was invoked!";
     }
 
 }
